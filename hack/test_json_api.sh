32bab51b7614bc329612e6d6dcd81c7897da5371
Georg Schreiber <georg.g.schreiber@daimler.com>
2020-11-12 11:03:28 +0100

    added test for /api/news JSON API

---
+++ hack/test_json_api.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+function cleanup()
+{
+  echo "stopping web-cli"
+  kill "$PID"
+}
+
+trap cleanup EXIT
+nohup go run ./ serve &
+PID="$!"
+
+# wait until web-cli seems ready
+while ! nc -z localhost 8080; do
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
+echo "running web-cli with PID: "$PID""
+
+
+echo "testing if json api is returning 5 news elements..."
+JSONNEWS=$(curl -s http://localhost:8080/api/news | jq '.news | length')
+
+if [ "$JSONNEWS" != 5 ]
+then
+  echo "failed to get news from 'api/news', got response: ""$JSONNEWS"
+  exit 1
+fi
+
+echo "success! :-)"
